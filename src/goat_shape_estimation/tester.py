0f87a47c5d6172e7368b949ae1be59219e0050c1
Valentin Yuryev <goodfaiter@gmail.com>
2025-07-24 11:42:21 +0200

    updated tester

--- src/goat_shape_estimation/tester.py
+++ src/goat_shape_estimation/tester.py
@@ -9,49 +9,52 @@ traced_model = torch.jit.load("/workspace/data/output/latest_lstm_model.pt", map
 # traced_model.reset()
 # data = pd.read_parquet("/workspace/data/2025_06_04/2025_06_04_16_24_22_goat_training.parquet") # c -> s -> c
 # data = pd.read_parquet("/workspace/data/2025_06_04/2025_06_04_15_52_28_goat_training.parquet") # yaw in circle mode
-data = pd.read_parquet("/workspace/data/2025_06_04/2025_06_04_15_49_09_goat_training.parquet") # c -> rover + drive
+data = pd.read_parquet("/workspace/data/2025_07_21/rosbag2_2025_07_21-14_16_19_goat_training.parquet")  # yaw in circle mode
+# data = pd.read_parquet("/workspace/data/2025_07_21/rosbag2_2025_07_22-08_50_04_goat_training.parquet") # drive in rover
 # data = pd.read_parquet("/workspace/data/2025_06_04/2025_06_04_15_50_40_goat_training.parquet") # rov -> c
 
-data_processor_goat = DataProcessorGoat()
+device = device = torch.device("cpu")
+
+data_processor_goat = DataProcessorGoat(device)
 inputs = data_processor_goat.process_input_data(data)
 targets = data_processor_goat.process_output_data(data)
 
-estimated_velocities = torch.zeros([inputs.shape[0], 6], dtype=torch.float, device=torch.device('cpu'))
+num_points = 12
+num_points_per_circle = 9
+points_index = 0
+estimated_points = torch.zeros([num_points, 3], dtype=torch.float, device=torch.device("cpu"))
+estimated_points_to_visualize = torch.zeros([2, num_points_per_circle, 3], dtype=torch.float, device=torch.device("cpu"))
+estimated_gravity = torch.zeros([3], dtype=torch.float, device=torch.device("cpu"))
+grav_index = points_index + num_points * 3
+estimated_velocities = torch.zeros([inputs.shape[0], 6], dtype=torch.float, device=torch.device("cpu"))
+vel_index = grav_index + 3
 
-for i in range(inputs.shape[0]):
+for i in range(inputs.shape[0] - 20):
     sample_input = inputs[i, :].unsqueeze(0).unsqueeze(0)
+
     with torch.no_grad():
-        output = traced_model(sample_input)
+        output = traced_model(sample_input).squeeze().squeeze()
 
     ## Points and Gravity
-    # if i % 100 == 0:
-    #     print(output.squeeze().squeeze().numpy())
-    #     print(targets[i, :].numpy())
-    #     # Get single new observation
-    #     new_data_point = sample_input.squeeze().squeeze().numpy()
-    #     vis_points = output[0, 0:24].detach().reshape(8, 3).numpy()
-    #     vis_points = np.append(vis_points, vis_points[0, :]) # Append first point to last to close the spline loop
-    #     down_vec = output[0, 24:27].detach().numpy()
-    #     # target_data_point = targets[i, 0:24].detach().reshape(8, 3).numpy()
-    #     # down_vec = targets[i, 24:27].detach().numpy()
-    #     # vis_points = target_data_point
-    #     # vis_points = np.append(vis_points, vis_points[0, :]) # Append first point to last to close the spline loop
-        
-    #     visualize_3d_spline(
-    #         vis_points.reshape(9, 3),
-    #         down_vec,
-    #         text=f" with Tendon length {new_data_point[-2]} and {new_data_point[-1]}, iteration {i}",
-    #         filename="data/output/" + f"spline_{i}",
-    #     )
-    #     print(new_data_point[20:22])
+    if i % 100 == 0:
+        estimated_points = output[:grav_index].view(num_points, 3)
+        estimated_gravity = output[grav_index:vel_index]
+        estimated_points_to_visualize[0, :, :] = estimated_points[[0, 1, 2, 3, 4, 5, 6, 7, 0], :]
+        estimated_points_to_visualize[1, :, :] = estimated_points[[0, 8, 2, 9, 4, 10, 6, 11, 0], :]
 
-    # estimated_velocities[i, :] = output[0, 27:]
-    estimated_velocities[i, :] = output[0, :]
+        visualize_3d_spline(
+            estimated_points_to_visualize.detach().numpy(),
+            estimated_gravity.detach().numpy(),
+            text=f" with Tendon length {inputs[i, -2]:.2f}[m] and {inputs[i, -1]:.2f}[m], timestep {i*0.05:.2f}[s]",
+            filename="data/output/" + f"spline_{i}",
+        )
+
+    estimated_velocities[i, :] = output[vel_index:]
 
 ### Plot velocities
-plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 0], targets.detach().numpy()[:, 0])
-plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 1], targets.detach().numpy()[:, 1])
-plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 2], targets.detach().numpy()[:, 2])
-plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 3], targets.detach().numpy()[:, 3])
-plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 4], targets.detach().numpy()[:, 4])
-plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 5], targets.detach().numpy()[:, 5])
+# plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 0], targets.detach().numpy()[:, vel_index], title="Linear X")
+# plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 1], targets.detach().numpy()[:, vel_index + 1], title="Linear Y")
+# plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 2], targets.detach().numpy()[:, vel_index + 2], title="Linear Z")
+# plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 3], targets.detach().numpy()[:, vel_index + 3], title="Angular X")
+# plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 4], targets.detach().numpy()[:, vel_index + 4], title="Angular Y")
+# plot_velocity_comparison(estimated_velocities.detach().numpy()[:, 5], targets.detach().numpy()[:, vel_index + 5], title="Angular Z")
